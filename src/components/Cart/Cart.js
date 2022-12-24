41d7ff61c687fb1dad2a552eeeb33b475245aea8
Lore1ei <harubator@gmail.com>
2022-12-24 14:18:30 +0700

    clear cart and sending modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,41 @@
 import classes from './Cart.module.css'
 import Modal from "../UI/Modal";
 import CartContext from "../store/cart-context";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = props => {
-    const cartCtx = useContext(CartContext);
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-    const hasItems = cartCtx.items.length > 0;
+    const [isCheckout, setIsCheckout] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [didSubmit, setDidSubmit] = useState(false);
 
+    const cartCtx = useContext(CartContext);
     const itemAddHandler = item => {
         cartCtx.addItem({...item, amount: 1})
     }
-
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
     const itemRemoveHandler = id => {
         cartCtx.removeItem(id)
+    }
+
+    const orderHandler = () => {
+        setIsCheckout(true)
+    }
+
+    const submitOrderHandler = async (userData) => {
+        setIsSubmitting(true);
+        await fetch('https://reactfoodapp-c9892-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        });
+        setIsSubmitting(false);
+        setDidSubmit(true);
+        cartCtx.clearCart();
     }
 
     const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map(item =>
@@ -23,17 +44,33 @@ const Cart = props => {
                   onAdd={itemAddHandler.bind(null, item)} />
     )}</ul>;
 
+    const modalActions = (<div className={classes.actions}>
+        <button onClick={props.onHideCartHandler} className={classes['button--alt']}>Close</button>
+        {hasItems && <button onClick={orderHandler} className={classes.button}>Order</button>}
+    </div>)
+
+    const cartModalContent = <> {cartItems}
+    <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+    </div>
+    {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onHideCartHandler} />}
+    {!isCheckout && modalActions}
+    </>
+
+    const didSubmitModalContent = <> <p>Successfully sent the order</p>
+        <div className={classes.actions}>
+            <button onClick={props.onHideCartHandler} className={classes.button}>Close</button>
+        </div>
+    </>;
+
+    const isSubmittingModal =  <p>Sending order data</p>;
+
     return(
         <Modal onClose={props.onHideCartHandler}>
-            {cartItems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-            </div>
-            <div className={classes.actions}>
-                <button onClick={props.onHideCartHandler} className={classes['button--alt']}>Close</button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {!isSubmitting && !didSubmit && cartModalContent}
+            {isSubmitting && isSubmittingModal}
+            {!isSubmitting && didSubmit && didSubmitModalContent}
         </Modal>
     )
 }
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,63 @@
 import classes from './AvailableMeals.module.css'
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import {useEffect, useState} from "react";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+
 
 const AvailableMeals = props => {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+    const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
+
+    useEffect( () => {
+        setLoading(true);
+        const fetchMeals = async () => {
+            const response =  await fetch('https://reactfoodapp-c9892-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+
+            if(!response.ok){
+                throw new Error('Error')
+            }
+
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for(const key in responseData){
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                });
+            }
+            setMeals(loadedMeals);
+            setLoading(false)
+        }
+
+            fetchMeals().catch(error => {
+                setLoading(false);
+                setHttpError(error.message)
+            });
+    }, [])
+
+
+    if(loading){
+        return <section className={classes.mealsIsLoading}>
+            <p>Loading</p>
+        </section>
+    }
+    if(httpError){
+        return <section className={classes.mealsError}>
+            <p>{httpError}</p>
+        </section>
+    }
+
+    const mealsList = meals.map(meal => <MealItem key={meal.id} id={meal.id}
+    name={meal.name} description={meal.description} price={meal.price} />);
 
     return(
+
         <section className={classes.meals}>
             <Card>
                <ul>
@@ -40,6 +65,7 @@ const AvailableMeals = props => {
                 </ul>
             </Card>
         </section>
+
     )
 }
 
